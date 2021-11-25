cb83e2e509c0347ff61e1dff03d2fea1d6232d7e
Simo Siren <sirensimo7@gmail.com>
2021-11-25 10:42:10 +0200

    delte confirming phase 1

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from app.views import landingview
-from .views import landingview, productlistview, supplierlistview, addsupplier
+from .views import landingview, productlistview, supplierlistview, addsupplier, addproduct, deleteproduct
 
 urlpatterns = [
     path('', landingview),
 
     # Products url´s
     path('products/', productlistview),
+    path('add-product/', addproduct),
+    path('delete-product/<int:id>/', deleteproduct),
 
     # Supplier url´s
     path('suppliers/', supplierlistview),
--- app/views.py
+++ app/views.py
@@ -5,8 +5,29 @@ def landingview(request):
     return render(request, 'landingpage.html')
 
 # Product view´s
+
 def productlistview(request):
-    return render(request, 'productlist.html')
+    productlist = Product.objects.all()
+    supplierlist = Supplier.objects.all()
+    context = {'products': productlist, 'suppliers': supplierlist}
+    return render (request,"productlist.html",context)
+
+
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['packagesize']
+    c = request.POST['unitprice']
+    d = request.POST['unitsinstock']
+    e = request.POST['supplier']
+    
+    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def deleteproduct(request, id):
+    Product.objects.filter(id = id).delete()
+    return redirect(request.META['HTTP_REFERER'])
+
 
 # Supplier view´s
 def supplierlistview(request):
