649cad103635a1f496c267c2fccb1ff08ecb6469
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2021-11-30 06:44:12 +0200

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,2 @@
-# Jos rekisteröi tällä tavalla adminille oman appin
-# modelit, voi myös admin sivuilta muokata näitä tietoja.
 
 from django.contrib import admin
-
-from app.models import Supplier, Product
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Supplier)
-class SupplierAdmin(admin.ModelAdmin):
-    pass
